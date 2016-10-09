72e72b9708fd39d43683250d84e97054b1dd4305
jobiols <jorge.obiols@gmail.com>
2016-10-08 21:23:44 -0300

    [REF]  odoo rpc

--- update_mila_prices/update_mila_prices.py
+++ update_mila_prices/update_mila_prices.py
@@ -6,13 +6,10 @@ import csv
 
 import odoorpc
 
-
-
-# PASSWORD = raw_input('Enter password: ')
-PASSWORD = 'melquiades'
+PASSWORD = raw_input('Enter password: ')
 
 login = {
-    'server': 'localhost',
+    'server': '52.205.148.95',
     'port': 8068,
     'database': 'makeover_datos',
     'username': 'admin',
@@ -29,6 +26,23 @@ csv_params = {
     'purchase_ok': 5
 }
 
+problemas_csv = [
+    '2510P-',
+    '2813P-',
+]
+
+problemas_odoo = [
+    '2010P',  # que es esto???
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+]
 
 class product:
     def __init__(self, line, parent):
@@ -62,9 +76,23 @@ class product:
         else:
             return False
 
+    def smart_fix(self, dc):
+        """ Este fix corrige todas las macanas que se mandan en mila
+        """
+
+        # cambiar punto por menos, si es el último caracter solo eliminar
+        if dc[-1] == '.':
+            dc = dc.replace('.', '')
+        dc = dc.replace('.', '-')
+
+        # los codigos no pueden contener espacios
+        dc = dc.replace(' ', '')
+
+        return dc
+
     def attr(self, name):
         if name == 'default_code':
-            return self._line[self._parent._params[name]][1:-1]
+            return self.smart_fix(self._line[self._parent._params[name]][1:-1] )
         elif name == 'category':
             return self._map_category(self._line[self._parent._params[name]])
         elif name == 'sale_ok' or name == 'purchase_ok':
@@ -96,8 +124,22 @@ class csv_file:
                 list.append(product(line, self))
         return list
 
+####################################################################################
+
 # leer el archivo csv
 file = csv_file(csv_params)
+
+"""
+aa = []
+for rec in file.obj():
+    aa.append(rec.attr('default_code'))
+
+for r in aa:
+    if ' ' in r:
+        print r
+
+exit()
+"""
 
 # conectar con odoo, proveer credenciales
 odoo = odoorpc.ODOO(login.get('server'), port=login.get('port'))
@@ -112,11 +154,26 @@ odoo.login(login.get('database'), login.get('username'), login.get('password'))
 # obtener objeto product
 prod_obj = odoo.env['product.product']
 
+ids = prod_obj.search(['|', '|', '|',
+                       ('categ_id', '=', 25),
+                       ('categ_id', '=', 26),
+                       ('categ_id', '=', 45),
+                       ('categ_id', '=', 49)
+                       ], order='default_code')
+res = []
+for prod in prod_obj.browse(ids):
+    res.append(prod.default_code)
+
+for pro in res:
+    if pro[-1] == '-':
+        print pro
+
+
 exit()
 
 # por cada linea del archivo mila hacer
 for prod in file.obj():
-    if False:
+    if True:
         print prod.attr('category')
         print prod.attr('default_code')
         print prod.attr('name')
@@ -124,8 +181,15 @@ for prod in file.obj():
         print prod.attr('sale_ok')
         print prod.attr('purchase_ok')
 
-    ids = prod_obj.search([('default_code', '=', prod.attr('default_code'))])
+    ids = prod_obj.search([('default_code', 'like', prod.attr('default_code'))])
     recordset = prod_obj.browse(ids)
+
+    if len(recordset) > 1:
+        print '--------------------------'
+        for rec in recordset:
+            print rec.name
+
+    exit()
 
 
     # el producto ya existe en odoo
